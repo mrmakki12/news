5d5264ff63e343bc0270afbaaf2fda28a692cbf4
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-13 16:52:43 -0400

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 // import components/pages here
 import { Header } from './Components/Header.js';
+import { Footer } from './Components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer = () => {
+
+    return (
+        <footer>
+            <p>DESIGNED WITH LOVE BY <span><a href="https://tyreeckcodes.com" target='_blank'>TYREECK MAKKI 2022.</a></span></p>
+        </footer>
+    )
+}
