23881bc415db1212a8cdf016824e53cd7fa870f4
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-13 17:51:59 -0400

    setting up react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 // import components/pages here
 import { Header } from './Components/Header.js';
 import { Footer } from './Components/Footer.js';
+// router components
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
