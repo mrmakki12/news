cfc507f9bfb6a309d31bf1127ed3b811fda5c623
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-03-14 16:37:32 -0400

    Setting up routing and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 // import components/pages here
 import { Header } from './Components/Header.js';
 import { Footer } from './Components/Footer.js';
+import { ArticlePreviews } from './Components/ArticlePreviews/ArticlePreviews.js';
 // router components
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/articles' element={<ArticlePreviews />} />
+          <Route exact path='/articles/:id' element={<Article />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
